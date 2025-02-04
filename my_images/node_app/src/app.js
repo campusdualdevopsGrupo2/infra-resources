a5bf294be658259f7d2691e19258ab6be14a8eeb
andycampusdual <andy.garcia@campusdual.com>
2025-02-04 19:23:22 +0000

    feat(update): Update node app

--- my_images/node_app/src/app.js
+++ my_images/node_app/src/app.js
@@ -3,10 +3,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname, 'public')));
-
-=======
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,7 +11,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
->>>>>>> d96b3c10530d1200dc9049368c61f3be60020f37
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
