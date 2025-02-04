612132c92c892085120f34447f70c64bb3852a5d
dalessi <danialessi93@hotmail.com>
2025-02-04 20:47:08 +0100

    Crear pruebas unitarias para la aplicación Node.js

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-# infra-resourses
-=======
 # infra-resources
-
->>>>>>> 9df70e41ea8205fded5bccd3b9353d9620f9877d
--- my_images/node_app/src/app.js
+++ my_images/node_app/src/app.js
@@ -1,16 +1,24 @@
+// src/app.js
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Default route to serve index.html
+// Simple route to serve 'index.html'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// Export the app for testing purposes
+module.exports = app;
+
+// The server should not be started here in the test environment
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
