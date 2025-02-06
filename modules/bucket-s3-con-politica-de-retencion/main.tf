0b36a715ce72f61c97734f8136e2a3c840c04dc6
Jorge382 <jorge.vidal@campusdual.com>
2025-02-06 19:17:13 +0100

    Modificación bucket s3 con política de retención

--- modules/bucket-s3-con-politica-de-retencion/main.tf
+++ modules/bucket-s3-con-politica-de-retencion/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name  # Usamos el nombre del bucket desde una variable
 
   lifecycle {
-    prevent_destroy = false  # Allow the bucket to be destroyed
+    prevent_destroy = false  # Permite que el bucket sea destruido
   }
 }
 
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   bucket = aws_s3_bucket.my_bucket.bucket
 
   versioning_configuration {
-    status = "Suspended"  # 'Suspended' if versioning is to be disabled
+    status = "Suspended"  # 'Suspended' para deshabilitar el versionado
   }
 }
 
@@ -35,14 +35,42 @@ resource "aws_s3_bucket_website_configuration" "example" {
   index_document {
     suffix = "super.html"
   }
-
 }
+
+# Política del bucket para permitir el acceso público a los objetos
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Configuración para desactivar el bloqueo de acceso público (opcional)
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 terraform {
   backend "s3" {
-    bucket = "proyecto-devops-grupo-dos"          # Nombre de tu bucket S3
-    key    = "s3-práctica-6-2-2025/terraform.tfstate"           # Ruta y nombre del archivo de estado dentro del bucket
-    region = "eu-west-2"                           # Región donde está tu bucket S3
-    encrypt = true                                   # Habilita el cifrado en el bucket
+    bucket  = "proyecto-devops-grupo-dos"          # Nombre de tu bucket S3
+    key     = "s3-práctica-6-2-2025/terraform.tfstate"  # Ruta y nombre del archivo de estado dentro del bucket
+    region  = "eu-west-2"                           # Región donde está tu bucket S3
+    encrypt = true                                  # Habilita el cifrado en el bucket
   }
 }
 
