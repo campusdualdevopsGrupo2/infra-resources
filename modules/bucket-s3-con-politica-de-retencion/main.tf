eaf217d3bafe3f45b61efe7b6da2ac24ff0a1be3
Jorge382 <jorge.vidal@campusdual.com>
2025-02-06 15:46:54 +0100

    Bucket s3 con políica de retención

---
+++ modules/bucket-s3-con-politica-de-retencion/main.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name  # Usamos el nombre del bucket desde una variable
+
+  lifecycle {
+    prevent_destroy = false  # Allow the bucket to be destroyed
+  }
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+
+  versioning_configuration {
+    status = "Suspended"  # 'Suspended' if versioning is to be disabled
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    id     = "rule-1"
+    status = "Enabled"
+
+    expiration {
+      days = 30  # Eliminar objetos después de 30 días
+    }
+
+    # Puedes añadir otras acciones de transición/expiración aquí
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "proyecto-devops-grupo-dos"          # Nombre de tu bucket S3
+    key    = "s3-práctica-6-2-2025/terraform.tfstate"           # Ruta y nombre del archivo de estado dentro del bucket
+    region = "eu-west-2"                           # Región donde está tu bucket S3
+    encrypt = true                                   # Habilita el cifrado en el bucket
+  }
+}
+
---
+++ modules/bucket-s3-con-politica-de-retencion/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region           = "eu-west-2" # Ajusta esto según tu región deseada
+bucket_name          = "practica-s3-con-politica-de-retencion"
+acl                  = "private" # Puedes cambiar esto si necesitas otro tipo de acceso
