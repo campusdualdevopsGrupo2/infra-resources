ac663c4fca6801a2b8d335120b161f261e5f7d11
Salvadortarrio <salvador.tarrio@campusdual.com>
2025-02-08 19:50:21 +0100

    modulo 3 clase 5 listo

---
+++ modulo3/clase5/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = local.aws_region  # Usamos una variable para la región, que podemos definir en variables.tf
+
+}
+locals{
+  aws_region           = "eu-west-2" # Ajusta esto según tu región deseada
+
+}
+
+module "ecs"{
+    source= "git@github.com:campusdualdevopsGrupo2/cloud-resources.git//fixed_resources/ecs"
+}
+
+module "ecs_recursos"{
+    source= "git@github.com:campusdualdevopsGrupo2/cloud-resources.git//modulo3/clase5/ec2_resources"
+    cluster_id=module.ecs.ecs_cluster_id
+}
+
--- modulo3/clase8/chat_app/main.tf
+++ modulo3/clase8/chat_app/main.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region = local.aws_region  # Usamos una variable para la región, que podemos definir en variables.tf
+
+}
+locals{
+  aws_region           = "eu-west-2" # Ajusta esto según tu región deseada
+
+}
+
 module "eks"{
     source= "git@github.com:campusdualdevopsGrupo2/cloud-resources.git//fixed_resources/eks"
 }
